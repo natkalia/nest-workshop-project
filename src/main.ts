71cf21c593fde80fadb05833346c611a5e010e79
Natalia Banasiewicz <natalia.banasiewicz@wp.pl>
2020-03-21 13:18:09 +0100

    Add and connect Nest

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter} from '@nestjs/platform-express'
+import {app as expressApp } from '../express/app';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
   await app.listen(3000);
 }
 bootstrap();
